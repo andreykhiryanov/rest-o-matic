0588b6e938fa4955eae79be8798f935ee15d2705
Andrey Khiryanov <andreykhiryanov@gmail.com>
2019-12-11 14:32:58 +0300

    Better tables

--- target/scala-2.13/twirl/main/views/html/home/visitorcard.template.scala
+++ target/scala-2.13/twirl/main/views/html/home/visitorcard.template.scala
@@ -40,9 +40,9 @@ Seq[Any](format.raw/*4.21*/("""
     <a class="btn btn-primary" href=""""),_display_(/*9.39*/routes/*9.45*/.VisitorController.editVisitor(visitor.getFirstName)),format.raw/*9.97*/("""">Edit Visitor</a> <a class="btn btn-danger" href=""""),_display_(/*9.149*/routes/*9.155*/.VisitorController.destroyVisitor(visitor.getFirstName)),format.raw/*9.210*/("""">Remove Visitor</a>
     <br><br>
 
-    <table border="1px solid grey" style="border-collapse: collapse">
+    <table class="table table-striped">
         <tr>
-            <th colspan="5" style="text-align:center">Information</th>
+            <th colspan="5">Information</th>
         </tr>
         <tr>
             <td>Last Name</td>
@@ -73,10 +73,10 @@ Seq[Any](format.raw/*4.21*/("""
 
               /*
                   -- GENERATED --
-                  DATE: 2019-12-11T12:56:57.718
+                  DATE: 2019-12-11T14:31:43.334
                   SOURCE: D:/Java/Projects/rest-o-matic/app/views/home/visitorcard.scala.html
-                  HASH: 3a166ffdf722afaff0a24655d5b40cdfd53d6429
-                  MATRIX: 656->1|685->25|1012->45|1126->64|1156->69|1192->97|1230->98|1262->104|1293->109|1308->116|1341->129|1422->184|1436->190|1508->242|1587->294|1602->300|1678->355|1978->628|1994->635|2027->647|2135->728|2151->735|2181->744|2289->825|2305->832|2341->847
+                  HASH: b559b1911ff8f3e2f287efdf89d240ce08b66695
+                  MATRIX: 656->1|685->25|1012->45|1126->64|1156->69|1192->97|1230->98|1262->104|1293->109|1308->116|1341->129|1422->184|1436->190|1508->242|1587->294|1602->300|1678->355|1922->572|1938->579|1971->591|2079->672|2095->679|2125->688|2233->769|2249->776|2285->791
                   LINES: 24->1|25->2|30->4|35->4|37->6|37->6|37->6|38->7|38->7|38->7|38->7|40->9|40->9|40->9|40->9|40->9|40->9|49->18|49->18|49->18|53->22|53->22|53->22|57->26|57->26|57->26
                   -- GENERATED --
               */
